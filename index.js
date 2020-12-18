6fa14212743aa82373c17cd50319ae05d7b3e510
gusrb8925 <gusrb8925@naver.com>
2020-12-18 23:52:33 +0900

    update nodemon 20-12-18

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-const express = require('express');
-const app = express();
-const port = 5000;
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://kkyu:mongo@boilerplate.deixg.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+.catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+  res.send('Hello World! nodemon test!!')
+})
+
+app.post('/register',(req,res)=>{
+
+    // 회원 가입 시 필요한 정보 DB에 넣기
+    const user = new User(req.body);
+    user.save((err,userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({ success: true})
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-});
+})
