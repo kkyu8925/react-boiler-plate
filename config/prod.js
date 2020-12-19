0365307d1bd3e22a43e8924498a5e9d7e516259a
gusrb8925 <gusrb8925@naver.com>
2020-12-19 17:04:55 +0900

    add config 20-12-19

---
+++ config/prod.js
@@ -0,0 +1,5 @@
+// 환경 변수 process.env.NODE_ENV
+// deploy(배포) 한후 
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://kkyu:mongo@boilerplate.deixg.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
